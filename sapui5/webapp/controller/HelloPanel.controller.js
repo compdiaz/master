4b4c0bc3162168f8a7e4f9d8421d705c3b50fc2f
compdiaz <compdiaz@gmail.com>
2024-04-13 16:45:31 +0000

    OPENDIALOG

--- sapui5/webapp/controller/HelloPanel.controller.js
+++ sapui5/webapp/controller/HelloPanel.controller.js
@@ -1,18 +1,21 @@
 //@ts-nocheck
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ],
 
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {sap.m.MessageToast} MessageToast
+     * @param {sap.ui.core.Fragment} Fragment
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Fragment) {
         "use strict"
         return Controller.extend("invoices.sapui5.controller.HelloPanel", {
             onInit: function () {
             },
+
 
 
             onShowHello: function () {
@@ -22,6 +25,30 @@ sap.ui.define([
                 var sRecipient = this.getView().getModel().getProperty("/recipient/name");
                 var sMsg = oBundle.getText("helloMsg", [sRecipient]);
                 MessageToast.show(sMsg);
+            },
+
+            onOpenDialog: function () {
+
+                const oView = this.getView();
+
+
+                if (!this.byId("helloDialog")) {
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "invoices.sapui5.View.HelloDialog"
+
+                    }).then(function (oDialog) {
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    });
+                } else {
+                    this.byId("helloDialog").open();
+                }
+            },
+            onCloseDialog: function () {
+                this.byId("helloDialog").close();
+
             }
+
         });
     });
